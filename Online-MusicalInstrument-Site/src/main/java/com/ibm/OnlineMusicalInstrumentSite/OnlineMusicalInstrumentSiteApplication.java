150c87d6a463cfb60999793f50f13dc45402ce2a
Neha179 <s7nehagupta.9@gmail.com>
2021-04-21 09:21:10 +0530

    OMS

---
+++ Online-MusicalInstrument-Site/src/main/java/com/ibm/OnlineMusicalInstrumentSite/OnlineMusicalInstrumentSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.OnlineMusicalInstrumentSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMusicalInstrumentSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMusicalInstrumentSiteApplication.class, args);
+	}
+
+}
---
+++ Online-MusicalInstrument-Site/src/test/java/com/ibm/OnlineMusicalInstrumentSite/OnlineMusicalInstrumentSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.OnlineMusicalInstrumentSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMusicalInstrumentSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
