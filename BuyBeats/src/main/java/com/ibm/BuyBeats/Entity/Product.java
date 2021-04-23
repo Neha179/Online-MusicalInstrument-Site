1df9735db9cff410b51285898156615b1cdb9dec
Neha179 <s7nehagupta.9@gmail.com>
2021-04-23 09:18:55 +0530

    Entities commit

---
+++ BuyBeats/src/main/java/com/ibm/BuyBeats/Entity/Product.java
@@ -0,0 +1,5 @@
+package com.ibm.BuyBeats.Entity;
+
+public class Product {
+
+}
--- BuyBeats/src/main/java/com/ibm/BuyBeats/OnlineMusicalInstrumentSiteApplication.java
+++ BuyBeats/src/main/java/com/ibm/BuyBeats/OnlineMusicalInstrumentSiteApplication.java
@@ -1,4 +1,4 @@
-package com.ibm.OnlineMusicalInstrumentSite;
+package com.ibm.BuyBeats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
