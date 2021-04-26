ccf833a351801aea5ba75b737eb91505f8655e90
Arya-P-Menon <aryas.pmenon@gmail.com>
2021-04-26 12:59:19 +0530

    relations

--- buybeats/src/main/java/com/ibm/buybeats/entity/Product.java
+++ buybeats/src/main/java/com/ibm/buybeats/entity/Product.java
@@ -9,11 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
--- buybeats/src/main/java/com/ibm/buybeats/entity/User.java
+++ buybeats/src/main/java/com/ibm/buybeats/entity/User.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
