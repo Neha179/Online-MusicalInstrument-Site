dee6d6ebea661e63cd2c3c534497fb07a7c2833b
Aakansha Arora <ibmjavafsdmr04@Javafsdnh264.iiht.tech>
2021-04-27 12:11:17 +0530

    package login

--- buybeats/src/main/java/com/ibm/buybeats/controller/AdminController.java
+++ buybeats/src/main/java/com/ibm/buybeats/controller/AdminController.java
@@ -1,5 +1,32 @@
 package com.ibm.buybeats.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.buybeats.entity.User;
+import com.ibm.buybeats.exception.EmailAlreadyExistsException;
+import com.ibm.buybeats.service.AdminService;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private AdminService adminService;
+	
+	@PostMapping(value="/login", consumes = "application/jsom")
+	public  String adminLogin(@RequestBody User u) {
+		try {
+			adminService.login(u);
+			return "Welcome Admin";
+		} catch (EmailAlreadyExistsException e) {
+			e.printStackTrace();
+			return "Not valid Email ID";
+		}
+	}
+	
 
 }
--- buybeats/src/main/java/com/ibm/buybeats/repository/UserRepository.java
+++ buybeats/src/main/java/com/ibm/buybeats/repository/UserRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ibm.buybeats.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+		public User findByEmailAndPassword(String email,String password);
+		
 		public User findByEmail(String email);
+		
+		
+
 }
