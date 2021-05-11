c70f1fead8bd6a5e021a5c39bb07d1eb12144a07
Monalisa Prusty <monalisaprusty15@gmail.com>
2021-05-11 18:42:03 +0530

    Completed User Registration

--- buybeats-react-app/src/App.js
+++ buybeats-react-app/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import AdminLogin from './Components/AdminLogin.js';
-import UserLogin from './Components/UserLogin';
+ import AdminLogin from './Components/AdminLogin.js';
+ import UserLogin from './Components/UserLogin';
+import UserRegistration from './Components/UserRegistration';
 
 function App() {
   return (
     <div className="App">
-      <AdminLogin/>
+      <AdminLogin />
       <UserLogin />
+      <UserRegistration />
     </div>
   );
 }
-
 export default App;
