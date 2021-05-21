e4b17c6aafb0138c77897ef6fd1841328a91726e
Monalisa <monalisaprusty15@gmail.com>
2021-05-21 13:26:44 +0530

    Admin CSS

--- buybeats-react-app/src/Components/AdminFullSearch.js
+++ buybeats-react-app/src/Components/AdminFullSearch.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import {BrowserRouter as Router,Switch,Route,Link, useParams,useRouteMatch} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route,Link, useParams,useRouteMatch, useHistory} from "react-router-dom";
 import AddProduct from './AddProduct';
 import AdminNameSearch from './AdminNameSearch';
 import AdminPidSearch from './AdminPidSearch';
 import ShowProduct from './ShowProduct';
+import '../CSS/AdminFullSearch.css';
+import {UseHistory} from 'react-router-dom';
 
 
 export default function AdminFullSearch(){
+
+    let history = useHistory();
+    const redirectTo = () =>
+    {
+        history.push("/adminLogin");
+    }
     function setLink(){
        <Link></Link>
     }
-    return(<Router>
-        
+    return(
+    <div className="background">
+        <br />
+        <button className="logout-button" onClick={redirectTo}>Logout</button>
+          <Router>
         <ul>
-            <li><Link to="/ByName">Search By Name</Link></li>
-            <li><Link to="/ByPid">Search and Update By Pid</Link></li>
-            <li><Link to="/showProduct">Show All Products</Link></li>
-            <li><Link to="/addProduct">Add New Product</Link></li>
+            <li><Link to="/ByName"><button className="button">Search By Name</button></Link></li>
+            <li><Link to="/ByPid"><button className="button">Search and Update By Pid</button></Link></li>
+            <li><Link to="/showProduct"><button className="button">Show All Products</button></Link></li>
+            <li><Link to="/addProduct"><button className="button">Add New Product</button></Link></li>
         </ul>
-        
-        
-        <Switch>
+        <br /><br /><br />
+          <Switch>
             <Route path="/ByName"><AdminNameSearch/></Route>
             <Route path="/ByPid"><AdminPidSearch/></Route>
             <Route path="/showProduct"><ShowProduct/></Route>
             <Route path="/addProduct"><AddProduct/></Route>
-            
         </Switch>
     </Router>
+    </div>
     );
 }
 
