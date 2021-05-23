6a22c9739734ee866ce2511b4326d0efa68074b4
Neha179 <s7nehagupta.9@gmail.com>
2021-05-23 10:21:38 +0530

    .

--- buybeats-react-app/src/Components/NewAddress.js
+++ buybeats-react-app/src/Components/NewAddress.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import '../CSS/NewAddress.css'
 import OrderService from '../Services/OrderService.js';
-
+import Button from './Button'
+import Select from 'react-select';
 export default class NewAddress extends Component {
     constructor(props) {
         super(props);
@@ -11,7 +13,10 @@ export default class NewAddress extends Component {
             city : '',
             state : '',
             pinCode : '',
-            addressType : ''
+            addressType : [
+                {label: 'Work', value:'Work'},
+                {label: 'Home', value: 'Home'}
+            ]
         }
     }
 
@@ -34,24 +39,35 @@ export default class NewAddress extends Component {
 
     render () {
         return (
+            <div className="newaddress">
+                <center><div className="box">
+                <h1 className="bigblue">New Address</h1>
             <form onSubmit={() => this.onSave()}>
-            <table class="table table-dark">
+            <table>
                 <tr><td>House Number</td>
-                    <td><input name="houseNumber" value={this.state.houseNumber} required onChange={this.handleInput} /></td></tr>
+                    <td><input name="houseNumber" className="cobtrol-label" value={this.state.houseNumber} pattern="[A-Z][a-zA-Z\s]*([\d]*)\/?([\d]*)?" required onChange={this.handleInput} /></td></tr>
+                <br />
                 <tr><td>Street</td>
-                    <td><input name="street" value={this.state.street} required onChange={this.handleInput} /></td></tr>
+                    <td><input name="street" className="control-label" value={this.state.street} pattern="[A-Z][a-zA-Z\s]*([\d]*)\/?([\d]*)?" required onChange={this.handleInput} /></td></tr>
+                <br />
                 <tr><td>City</td>
-                    <td><input name="city" value={this.state.city} required onChange={this.handleInput} /></td></tr>
+                    <td><input name="city" className="control-label" value={this.state.city} pattern="[A-Z][a-z]{3,}" required onChange={this.handleInput} /></td></tr>
+                <br />
                 <tr><td>State</td>
-                    <td><input name="state" value={this.state.state} required onChange={this.handleInput} /></td></tr>
+                    <td><input name="state" className="control-label" value={this.state.state} pattern="[A-Z][a-z]{4,}" required onChange={this.handleInput} /></td></tr>
+                
                 <tr><td>PIN</td>
-                    <td><input name="pinCode" value={this.state.pinCode} pattern="[0-9]{6}" required onChange={this.handleInput} /></td></tr>
+                    <td><input name="pinCode" className="control-label" value={this.state.pinCode} pattern="[0-9]{6}" required onChange={this.handleInput} /></td></tr>
+                
                 <tr><td>Address Type</td>
-                    <td><input name="addressType" value={this.state.addressType} required onChange={this.handleInput} /></td></tr>
-                <tr><td><button class="btn btn-primary" type="submit">Save Address</button></td>
-                    <td><button class="btn btn-warning" onClick={() => this.onCancel()}>Cancel Address</button></td></tr>
+                
+                <Select options={this.state.addressType} />
+                    <td></td></tr>
+                <tr><td><Button class="btn btn-primary" type="submit">Save Address</Button></td>
+                    <td><Button class="btn btn-warning" onClick={() => this.onCancel()}>Cancel Address</Button></td></tr>
             </table>
-            </form>
+            </form></div></center>
+            </div>
         )
     }
 }
